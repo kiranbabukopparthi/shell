3aff6559ff3bafe3b34bf1cf80401cda4e080f1d
Kopparthi Kiran Babu <thekiranbabukopparthi@gmail.com>
2026-02-27 16:05:38 +0530

    changes done

--- variables.sh
+++ variables.sh
@@ -23,8 +23,23 @@
 # read function will be learned later. Now we will use special varibles
 
 # program-3
-person1="$1"
-person2="$2"
+# person1="$1"
+# person2="$2"
+
+# echo "$person1 : How are you $person2"
+# echo "$person2 : I am fine. Thanks for asking $person1"
+
+# Here $1 indicates first variable and $2 incdicates second varibale (which are given at run time) - and so on
+# we have more special variables like these mentioned below
+
+we can access the data or can take the input from end user at run time usinh read function. 
+read function is used to read the data/inputb given by user and we can store that data using variables
+
+program-4
+echo "Please enter person1 name : "
+read $person1
+echo "Please enter person2 name : "
+read $person2
 
 echo "$person1 : How are you $person2"
 echo "$person2 : I am fine. Thanks for asking $person1"
