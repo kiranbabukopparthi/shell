06f87903846f62cd8538ab4ed242822b016a5440
Kopparthi Kiran Babu <thekiranbabukopparthi@gmail.com>
2026-02-27 18:22:38 +0530

    changes done

--- conditions.sh
+++ conditions.sh
@@ -3,10 +3,20 @@
 # IF is the conditional statement to execute the code.
 # To run the script bases on specific condition, we use if statememt
 
-condition="true"
-if [ condition==true ] ; then
- echo "if condition executed"
+# program-1
+# condition="true"
+# if [ condition==true ] ; then
+#  echo "if condition executed"
+# else
+#  echo "else condition is executed"
+#  fi
+
+# program-2
+userid=$(id -u)
+if [ userid -nq 0 ]; then
+ echo "Run with sudo access"
 else
- echo "else condition is executed"
- fi
- 
+ echo "You are running with sudo access"
+fi
+
+dnf install nginx -y
