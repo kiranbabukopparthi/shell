b4d86d9b5754bf280d720e91d032e7527d9e6b58
Kopparthi Kiran Babu <thekiranbabukopparthi@gmail.com>
2026-03-02 16:41:18 +0530

    updated

---
+++ errorhandling.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set command
+# set -e
+# To validate the errors we can use set command
+# set command will check errors and if error is there then it will exit and it will give the line number details of that error.
+
+# trap command
+# trap command in linux is a shell built in that defines and activates handlers to execute command when the shell receives a specific signal or condition.
+
+# set modifies shell behavior and error handling, 
+# while trap catches signals and runs commands when specific events occur.
+
+set -e
+echo "This is set command and below will get error"
+echoo "This is error"
